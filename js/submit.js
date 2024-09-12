6cebdcd801d4d9a2032a4c707772358adc428e46
rajkunwar214 <rajkunwar8853@gmail.com>
2024-09-12 17:39:41 +0530

    Fast Yatra

---
+++ js/submit.js
@@ -0,0 +1,7 @@
+var sub=document.getElementById("submit");
+sub.addEventListener("click",function(){
+  window.location.href = "submition.html"
+  alert("Thanks for contacting us! we will be in touch with you shortly.");
+
+  
+})
